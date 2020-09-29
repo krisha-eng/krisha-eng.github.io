c8ff637d6aa53592a0bc0530bac94bbd2446c1c5
Engineer <krisha-sarju.engineer@capgemini.com>
2020-09-29 11:00:11 +0530

    Add existing files

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function() {
+	"use strict";
+
+	sap.ui.require([
+		"opensap/W2/W2/test/integration/AllJourneys"
+	], function() {
+		QUnit.start();
+	});
+});
